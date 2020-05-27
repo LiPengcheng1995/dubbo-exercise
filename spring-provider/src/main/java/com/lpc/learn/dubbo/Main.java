f90bcbd3523f9937af2cd3cddfb2fa1c5ae34e69
lipengcheng3 <lipengcheng3@jd.com>
2020-05-27 18:44:27 +0800

    init

---
+++ spring-provider/src/main/java/com/lpc/learn/dubbo/Main.java
@@ -0,0 +1,13 @@
+package com.lpc.learn.dubbo;
+
+/**
+ * Package: com.lpc.learn.dubbo
+ * User: 李鹏程
+ * Email: lipengcheng3@jd.com
+ * Date: 2020/5/27
+ * Time: 18:30
+ * Description:
+ */
+public class Main {
+
+}
