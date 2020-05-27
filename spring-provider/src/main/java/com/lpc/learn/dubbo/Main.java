8472e6f7a096c78b99ee1949ebbd12ce3325ca11
lipengcheng3 <lipengcheng3@jd.com>
2020-05-27 19:47:24 +0800

    init

--- spring-provider/src/main/java/com/lpc/learn/dubbo/Main.java
+++ spring-provider/src/main/java/com/lpc/learn/dubbo/Main.java
@@ -1,5 +1,9 @@
 package com.lpc.learn.dubbo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Package: com.lpc.learn.dubbo
  * User: 李鹏程
@@ -7,7 +11,20 @@ package com.lpc.learn.dubbo;
  * Date: 2020/5/27
  * Time: 18:30
  * Description:
+ * @date 2020-05-27 19:47
+ * @author lipengcheng3
  */
+@Slf4j
+@SpringBootApplication
 public class Main {
-
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+        while (true) {
+            try {
+                Thread.currentThread().wait();
+            } catch (InterruptedException e) {
+                log.error("阻塞被打断,",e);
+            }
+        }
+    }
 }
